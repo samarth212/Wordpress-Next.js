b33fae91864bf305dc01a1abe02768d83081ee96
Samarth Kolanupaka <samarth212@gmail.com>
2026-02-13 17:04:05 -0500

    chore(next): set outputFileTracingRoot

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/app",
   allowedDevOrigins: ["https://seedmoney.org", "http://localhost:3001"],
+  outputFileTracingRoot: path.join(__dirname),
 };
 
 module.exports = nextConfig;
